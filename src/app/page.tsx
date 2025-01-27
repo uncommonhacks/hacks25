91fb3b8d4b22294de04fc2a228170987882ec4b4
Antrita Manduva <amanduva@uchicago.edu>
2025-01-27 15:20:53 -0600

    trying out static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
+
+// const imageStyle = {
+//     borderRadius: '10%',
+//     border: '1px solid #fff',
+// }
 
 export default function Home() {
   return (
-      <main className="relative">
+      <main>
         <div>
-          <h1> Hello, Uncommon Hacks! </h1>
+            <Image
+                className="tv-image"
+                src="/tv.svg"
+                alt="TV"
+                width={210}
+                height={170}
+                // onLoad={(e) => console.log(e.target.)}
+                // style={imageStyle}
+            />
         </div>
       </main>
   );
