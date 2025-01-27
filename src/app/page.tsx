6162e5589199372cd0881cf0921849e56524dd30
Antrita Manduva <amanduva@uchicago.edu>
2025-01-27 13:29:36 -0600

    need to add sidebar

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="relative">
+        <div>
+          <h1> Hello, Uncommon Hacks! </h1>
+        </div>
+      </main>
+  );
+}
